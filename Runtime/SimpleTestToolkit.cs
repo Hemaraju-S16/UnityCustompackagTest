d35c35b161ff1fee77e20eb73b02cdce349cf139
Hemaraju S <95607299+Hemaraju-S16@users.noreply.github.com>
2023-12-12 13:05:00 +0530

    added runtime folder

---
+++ Runtime/SimpleTestToolkit.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomToolkit
+{
+    public class SimpleTestToolkit : MonoBehaviour
+    {
+        public int maxCount = 100;
+        public const String classTag = "master";
+    }
+}
+
